0c739a6305b7ba40ecb3e51cfed33d5fe646cb8a
thiwankadilshanliyanage <thiwankadilshanhu98@gmail.com>
2026-05-08 15:16:25 +0900

    Fix brevo

--- MyOpenJournal/backend/src/utils/email.js
+++ MyOpenJournal/backend/src/utils/email.js
@@ -1,64 +1,34 @@
 import nodemailer from "nodemailer";
-import dns from "dns";
-
-dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
 
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
 
   port: 587,
 
   secure: false,
 
-  requireTLS: true,
-
-  family: 4,
-
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.BREVO_EMAIL,
+    pass: process.env.BREVO_SMTP_KEY,
   },
-
-  tls: {
-    rejectUnauthorized: false,
-    minVersion: "TLSv1.2",
-  },
-
-  connectionTimeout: 20000,
-  greetingTimeout: 20000,
-  socketTimeout: 30000,
 });
 
 export const sendEmail = async (to, subject, text) => {
 
   try {
 
-    console.log("EMAIL_USER loaded:",
-      process.env.EMAIL_USER ? "YES" : "NO"
-    );
-
-    console.log("EMAIL_PASS loaded:",
-      process.env.EMAIL_PASS ? "YES" : "NO"
-    );
-
     console.log("📨 Sending email to:", to);
-
-    // verify smtp connection
-    await transporter.verify();
-
-    console.log("✅ SMTP server ready");
 
     const info = await transporter.sendMail({
 
-      from: `"MyOpenJournal" <${process.env.EMAIL_USER}>`,
+      from: `"MyOpenJournal" <${process.env.BREVO_EMAIL}>`,
 
       to,
 
       subject,
 
       text,
-
     });
 
     console.log("✅ Email sent:", info.messageId);
